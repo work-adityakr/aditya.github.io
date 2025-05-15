77fa1bb69415340f40f5c000cdbd3a7b28100c18
Aditya kumar <149687071+work-adityakr@users.noreply.github.com>
2025-05-15 22:29:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let modal=document.querySelector('.container2');
+function openmod() {
+    modal.classList.add("containeractive");
+}
+
+function closemod() {
+    modal.classList.remove("containeractive");
+}
